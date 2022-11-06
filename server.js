a6ff000212f1bc2152d844a2688d529c0e2465e7
inedumozey <inedumozey@gmail.com>
2022-11-06 01:31:07 -0800

    initial commit

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 const express = require('express');
+const cors = require('cors');
+const weather = require('openweather-apis');
 const geoip = require('geoip-lite');
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(cors())
 
-    console.log("workingd")
+
+app.get('/', async (req, res) => {
+    try {
+        const info = {}
+        const ip = req.headers['ip']
+        if (!ip) {
+            res.status(400).json({ msg: 'Failed to fetch' })
+        }
+        else {
+            const geoInfo = geoip.lookup(ip)
+
+            info.ip = ip;
+            info.country = geoInfo.country;
+            info.latitude = geoInfo.ll[0]
+            info.longitude = geoInfo.ll[1];
+            info.city = geoInfo.city;
+
+            weather.setCoordinate(info.latitude, info.longitude)
+            // weather.setCity('Plateau');
+
+            weather.setAPPID(process.env.WEATHER_API);
+            weather.getAllWeather(function (err, JSONObj) {
+                info.temp = JSONObj.main.temp;
+                info.wind_speed = JSONObj.wind.speed;
+
+                res.status(200).json({ msg: 'successfull', data: info })
+            });
+        }
+    }
+    catch (err) {
+        res.status(500).json({ msg: err.message || 'Server error' })
+    }
 })
-
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
